e38a2cf9bca75d82ba21a56429bc342bb3bc3253
Dendie <dendie.sanjaya@gmail.com>
2023-01-15 23:51:43 +0700

    update add belanja

--- src/component/detailProduct/index.js
+++ src/component/detailProduct/index.js
@@ -18,16 +18,16 @@ import axios from "axios";
 import { dataProducts } from "../../utils/static";
 import { headerAxios } from "../../utils/headersAxios";
 import { currencyFormat } from "../../utils/functions";
-import { Link as RouterLink} from "react-router-dom";
-
-
+import { Link as RouterLink, useNavigate} from "react-router-dom";
 
 export default function DetailProductComponent() {
   const params = useParams();
-  const [qty, setQty] = useState(0);
+  const [qty, setQty] = useState(1);
   const [dataProducts, setDataProducts] = useState([]);
   const [loading, setLoading] = useState(null);
   const [dataDetail, setDataDetail] = useState(null);
+  const navigate = useNavigate();
+  var mychart = [];
 
   // useEffect(() => {
   //   updateState();
@@ -56,8 +56,8 @@ export default function DetailProductComponent() {
       newQty++;
     }
 
-    if (newQty < 0) {
-      newQty = 0;
+    if (newQty < 1) {
+      newQty = 1;
     }else if( newQty > dataDetail.qty){
       newQty = dataDetail.qtyl
     }
@@ -65,28 +65,79 @@ export default function DetailProductComponent() {
     setQty(newQty);
   };
 
+  /*
   const updateCart = () => {
-    let cart = JSON.parse(localStorage.getItem("cart"));
-    let newDataDetail = {};
-    if (!cart) {
-      cart = [];
-      newDataDetail = dataDetail;
+    let cart = localStorage.getItem("cart");
+    let cart_new = [];
+    if(!cart || cart.length < 1) {
+      cart_new.push(JSON.stringify(dataDetail));
     } else {
-      let newCart = cart.filter((val) => val.id == dataDetail.id);
-      if (newCart.length > 0) {
-        newDataDetail = newCart;
-      } else {
-        newDataDetail = dataDetail;
-      }
+      cart_new = localStorage.getItem("cart");
+      let newCart = cart_new.filter((val) => val.id == dataDetail.id);
+      console.log(newCart);
     }
-    newDataDetail.qty = qty;
-    let allCart = cart.filter((val) => val.id != dataDetail.id);
-    allCart.push(newDataDetail);
 
-    localStorage.setItem("cart", JSON.stringify(allCart));
     alert("Berhasil ditambahkan ke Cart");
+    //navigate("/product");
   };
+*/  
 
+
+const updateCart = () => {
+  mychart = [];
+  let mycart_new = [];
+
+  let cart_exsiting = localStorage.getItem("cart");
+  if(cart_exsiting == null || cart_exsiting.length < 1 ) {
+    dataDetail.qty = qty;
+    mychart.push(dataDetail);    
+    localStorage.setItem("cart",JSON.stringify(mychart));
+    //console.log(cart_exsiting);
+  } else {
+    let cart_exsiting_parsing = JSON.parse(cart_exsiting);
+    //mycart_new.push(cart_exsiting_parsing);
+    //console.log(mycart_new)
+    for (let j = 0; j < cart_exsiting_parsing.length; j++){     
+      if(dataDetail.id == cart_exsiting_parsing[j].id) {
+        console.log('data sudah ada update qty ' + cart_exsiting_parsing[j].name);
+        cart_exsiting_parsing[j].qty = (cart_exsiting_parsing[j].qty + qty);  
+        mycart_new.push(cart_exsiting_parsing[j]);   
+      } else {
+        let cek_exist = false;
+        for(let k = 0; k < mycart_new.length  ; k++ )  {
+          if(mycart_new[k].id == dataDetail.id) {
+            cek_exist = true;
+          }
+        }
+
+        let cek_exist_2 = false;
+        for (let l = 0; l < cart_exsiting_parsing.length; l++){ 
+          if(cart_exsiting_parsing[l].id == dataDetail.id) {
+            cek_exist_2 = true;
+          }
+        }   
+
+        if(cek_exist == false && cek_exist_2 == false) {
+          console.log('data baru ' + dataDetail.name)
+          dataDetail.qty = qty;         
+          mycart_new.push(dataDetail); 
+          mycart_new.push(cart_exsiting_parsing[j]);
+        }  else {
+          mycart_new.push(cart_exsiting_parsing[j]);
+        }
+        
+      }
+    }  
+
+    localStorage.setItem("cart",JSON.stringify(mycart_new));
+    console.log(mycart_new);
+
+  }
+
+ 
+  alert("Berhasil ditambahkan ke Cart");
+  navigate("/product");
+};
   const getDataProduct = async() =>{
     const url_api = "http://localhost:3000";
 
@@ -156,7 +207,7 @@ export default function DetailProductComponent() {
                 </Button>
                 <Typography
                   sx={{
-                    borderBottom: 1,
+                    borderBottom: 5,
                     width: 50,
                     mx: 1,
                     textAlign: "center",
